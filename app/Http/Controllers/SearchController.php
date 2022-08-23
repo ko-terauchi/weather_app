1464bf3d1c4bf5dd49b042c34441566d3f5802b4
ko-terauchi <ko-terauchi@r09.jp>
2022-08-23 08:58:50 +0000

    fix routes searchController

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    public function index() {
-        $prefs = config('pref');
+    public function prefPullDown() {
+        $prefs = config('pref');prefPullDown
         return view('search')->with(['prefs' => $prefs]);
     }
 }
